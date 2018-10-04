3a0cfaf97062359eb078976f1b7713019f10a1c0
gebgebgeb <asdf>
2018-10-04 15:25:43 -0400

    adding features

--- features.py
+++ features.py
@@ -2,9 +2,11 @@ def vecify(song, difficulty):
     chart_data = song['charts'][difficulty]
 
     bpm = list(song['bpms'].values())[0]
+    effective_bpm = bpm*chart_data['most_common_strm_res']
     out = [bpm
-            , bpm**2
-            , bpm**.5
+            , effective_bpm
+            , effective_bpm**.5
+            , effective_bpm**2
             , float(chart_data['num_notes']) / chart_data['num_measures']
             , chart_data['num_notes']
             , chart_data['num_notes']**.5
@@ -13,6 +15,7 @@ def vecify(song, difficulty):
             , chart_data['num_measures_stream']
             , chart_data['num_measures_stream']**.5
             , chart_data['num_measures_rest']
+            , chart_data['num_measures_interesting']
             , chart_data['longest_stream']
             , chart_data['longest_stream']**.5
             ]
--- model.py
+++ model.py
@@ -1,5 +1,27 @@
 from pprint import pprint
 
+def has_step(line):
+    return any([x in line for x in ['1','2','4']])
+
+def is_stream(measure):
+    if all([has_step(line) for line in measure]):
+        if len(measure) >= 12:
+            return True
+    return False
+
+def is_rest(measure):
+    if sum([has_step(line) for line in measure]) <= 8:
+        return True
+    return False
+
+def is_interesting(measure):
+    if len(measure) - sum([has_step(line) for line in measure]) <= 2:
+        return False
+    if is_rest(measure):
+        return False
+    return True
+
+
 def read_metadata(lines):
     credit = ''
     title = ''
@@ -37,15 +59,15 @@ def read_metadata(lines):
             }
 
 def read_chart(lines, target_difficulty):
+    output = {}
+
     reading = False
-    found = False
     measures = [] # list of lists, each sublist is a measure
     for line_idx, line in enumerate(lines):
         if line.startswith('#NOTES:'):
             difficulty = lines[line_idx + 3].strip().strip(':')
             if difficulty == target_difficulty:
                 reading = True
-                found = True
                 dance_type = lines[line_idx + 1].strip().strip(':')
                 rating = int(float(lines[line_idx + 4].strip().strip(':')))
                 cur_measure = []
@@ -57,39 +79,53 @@ def read_chart(lines, target_difficulty):
             if line.startswith(','):
                 measures.append(cur_measure)
                 cur_measure = []
-    if found:
+    if measures:
+        output['rating'] = rating
+        output['dance_type'] = dance_type
+        output['difficulty'] = target_difficulty
+        output['measures'] = measures
+
         num_measures = 0
         num_notes = 0
         for measure in measures:
             for line in measure:
                 num_notes += sum([x in ['1', '2', '4'] for x in line])
             num_measures += 1
+        output['num_notes'] = num_notes
+        output['num_measures'] = num_measures
 
         num_measures_stream = 0
         num_measures_rest = 0
+        num_measures_interesting = 0
         longest_stream = 0
         cur_stream = 0
         for measure in measures:
-            if len(measure) >= 16 and all(['1' in line for line in measure]):
+            if is_stream(measure):
                 num_measures_stream += 1
                 cur_stream += 1
             else:
                 if cur_stream > longest_stream:
                     longest_stream = cur_stream
                 cur_stream = 0
-            if sum([('1' in line or '2' in line or '4' in line) for line in measure]) <= 8:
+            if is_rest(measure):
                 num_measures_rest += 1
+            if is_interesting(measure):
+                num_measures_interesting += 1
+        output['num_measures_stream'] = num_measures_stream
+        output['num_measures_rest'] = num_measures_rest
+        output['num_measures_interesting'] = num_measures_interesting
+        output['longest_stream'] = longest_stream
 
-        return {'num_notes': num_notes
-                , 'num_measures': num_measures
-                , 'rating': rating
-                , 'difficulty': target_difficulty
-                , 'dance_type': dance_type
-                , 'num_measures_stream': num_measures_stream
-                , 'num_measures_rest': num_measures_rest
-                , 'longest_stream': longest_stream
-                , 'measures': measures
-                }
+        strm_res = {}
+        for measure in measures:
+            if is_stream(measure):
+                strm_res[len(measure)] = strm_res.setdefault(len(measure), 0) + 1
+        if strm_res:
+            most_common_res = sorted(strm_res, key=strm_res.get)[-1]
+        else:
+            most_common_res = 0
+        output['most_common_strm_res'] = most_common_res
+        return output
     else:
         return None
 
